780afddb170883ef53c0e19d27dac3b64480efc6
zrynuaa <zry_nuaa@163.com>
2018-06-11 18:41:16 +0800

    update

--- backend/servertest.go
+++ backend/servertest.go
@@ -8,24 +8,14 @@ import (
 
 func main()  {
 	//设置化学名与药品对应关系
-	d1 := based.Dose{Medicine_name:"mid1", Chemistry_name:"cid1", Medicine_amount:2, Medicine_price:1.2}
-	d2 := based.Dose{Medicine_name:"mid2", Chemistry_name:"cid1", Medicine_amount:1, Medicine_price:2.2}
-	d3 := based.Dose{Medicine_name:"mid3", Chemistry_name:"cid1", Medicine_amount:3, Medicine_price:0.9}
-	d4 := based.Dose{Medicine_name:"mid8", Chemistry_name:"cid2", Medicine_amount:2, Medicine_price:1.5}
-	d5 := based.Dose{Medicine_name:"mid9", Chemistry_name:"cid2", Medicine_amount:4, Medicine_price:0.78}
-	d6 := based.Dose{Medicine_name:"mid66", Chemistry_name:"cid8", Medicine_amount:2, Medicine_price:1.2}
-	var dose []based.Dose
-	dose = append(dose, d1, d2, d3, d4, d5, d6)
-	for _,v := range dose {
-		based.PutDose(v)
-	}
-
+	server.AddDoses()
 
 	//医院上传处方信息 上链
 	hp := server.HospitalPrescription{Hospital_id:"1234", Patient_id:"350622199009086758", Doctor_id:"67534", Disease:"上呼吸道感染", Policy:"hid1 OR (cid AND rid1)"}
 	var ch []server.Chemistry
 	ch = append(ch, server.Chemistry{Chemistry_name:"cid1",Amount:2})
 	ch = append(ch, server.Chemistry{Chemistry_name:"cid2",Amount:4})
+	ch = append(ch, server.Chemistry{Chemistry_name:"cid7",Amount:1})
 	hp.Chemistrys = ch
 
 	fmt.Println("医院处方信息:", hp)
@@ -39,9 +29,9 @@ func main()  {
 	//药店获取药品信息
 	fmt.Println("\n药店基本信息:")
 	drugstore1 := server.SetStore1Attrs()
-	fmt.Println(drugstore1, drugstore1.Doses[0].Mname)
+	fmt.Println(drugstore1, drugstore1.Doses)
 	drugstore2 := server.SetStore2Attrs()
-	fmt.Println(drugstore2, drugstore2.Doses[0].Mname)
+	fmt.Println(drugstore2, drugstore2.Doses)
 
 	fmt.Println("\n药店1能解密的信息:")
 	tr1 := server.StoregetMInfo(drugstore1)
@@ -57,8 +47,10 @@ func main()  {
 
 	//药店上传药品信息
 	server.StoresendTransaction(tr1[0])
-	server.StoresendTransaction(tr1[1])
-	server.StoresendTransaction(tr2[2])
+	server.StoresendTransaction(tr1[2])
+
+	server.StoresendTransaction(tr2[0])
+	server.StoresendTransaction(tr2[1])
 
 	fmt.Println("\n病人查看药品信息:")
 	_,trans := server.GetreadyInfo("Transaction",hp.Patient_id)
