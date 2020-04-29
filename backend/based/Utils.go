30c5cc34c6d0aa1538bf69c3c5e77566b52fe337
zrynuaa <zry_nuaa@163.com>
2020-04-30 00:00:15 +0800

    update SM-Collection

--- backend/based/Utils.go
+++ backend/based/Utils.go
@@ -2,28 +2,28 @@ package based
 
 import "strings"
 import (
+	"bytes"
 	"github.com/Doresimon/SM-Collection/SM3"
 	"strconv"
-	"bytes"
 )
 
 //need policy == hid* OR(cname* AND region1)
-func match(attr []string, policy string) bool{
+func match(attr []string, policy string) bool {
 	policy = strings.Replace(policy, " ", "", -1)
 	s := strings.Split(policy, "OR")
-	if isexist(attr, s[0]){
+	if isexist(attr, s[0]) {
 		return true
 	}
 	s2 := strings.Replace(s[1], "(", "", -1)
 	s2 = strings.Replace(s2, ")", "", -1)
-	s3 :=  strings.Split(s2, "AND")
+	s3 := strings.Split(s2, "AND")
 	if isexist(attr, s3[0]) && isexist(attr, s3[1]) {
 		return true
 	}
 	return false
 }
 
-func isexist(attr []string, a string) bool{
+func isexist(attr []string, a string) bool {
 	for _, i := range attr {
 		if i == a {
 			return true
@@ -32,19 +32,17 @@ func isexist(attr []string, a string) bool{
 	return false
 }
 
-func counthash(dataHash []byte,prevHash []byte,ts uint64,height int) []byte{
+func counthash(dataHash []byte, prevHash []byte, ts uint64, height int) []byte {
 	temp := dataHash
 	temp = append(temp, prevHash...)
 	temp = append(temp, []byte(strconv.Itoa(height))...)
 	return SM3.SM3_256(temp)
 }
 
-func splitBytesbyn(a []byte) [][]byte{
+func splitBytesbyn(a []byte) [][]byte {
 	return bytes.SplitN(a, []byte("\n\n"), -1)
 }
 
 func splitStringbyn(a string) []string {
 	return strings.SplitN(a, "\n\n", -1)
 }
-
-
